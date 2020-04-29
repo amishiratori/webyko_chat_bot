46dee6731f935e7da6179b5dfe4a38ff054c4176
amishiratori <shiratoriami@gmail.com>
2020-04-29 17:44:33 +0900

    send a message back

--- app.rb
+++ app.rb
@@ -8,14 +8,39 @@ require 'slack-ruby-client'
 
 Dotenv.load
 
-Slack.configure do |config|
-  config.token = ENV['SLACK_API_TOKEN']
-end
 
 post '/callback' do
   request_body = JSON.parse(request.body.read)
   case request_body['type']
   when 'url_verification'
     request_body['challenge']
+  when 'event_callback'
+    Slack.configure do |config|
+      config.token = ENV['SLACK_API_TOKEN']
+    end
+    if request_body['event']['channel'] == 'C012PCA7X1B'
+      message = request_body['event']['text']
+
+      request_content = {
+        'key' => ENV['USER_LOCAL_TOKEN'],
+        'message' => CGI.escape(message)
+      }
+      request_params = request_content.reduce([]) do |params, (key, value)|
+        params << "#{key}=#{value}"
+      end
+      chat_response = HTTP.get('https://chatbot-api.userlocal.jp/api/chat?' + request_params.join('&'))
+      return_result = JSON.parse(chat_response)
+      puts return_result
+
+      slack_response = HTTP.post(
+        'https://slack.com/api/chat.postMessage',
+        params: {
+          token: ENV['SLACK_API_TOKEN'],
+          channel: '#times_webyko',
+          text: return_result['result'],
+          as_user: true
+        }
+      )
+    end
   end
 end
