b92478c0b9c7af4c551233ee3e46e4785550e438
amishiratori <shiratoriami@gmail.com>
2020-05-04 21:43:00 +0900

    make reaction random

--- reactions.rb
+++ reactions.rb
@@ -9,12 +9,14 @@ Dotenv.load
 
 posts = Post.all
 posts.each do |post|
+  stamps = ['webyko_clap', 'webyko_good', 'webyko_heart', 'webyko_talk', 'webyko_sunny', 'webyko_balloon', 'webyko_surprise']
+  random = Random.new.rand(stamps.length)
   slack_uri = URI('https://slack.com/api/reactions.add')
   slack_res = Net::HTTP.post_form(
     slack_uri,
     'token' => ENV['SLACK_API_TOKEN'],
     'channel' => post.channel,
-    'name' => 'webyko_clap',
+    'name' => stamps[random],
     'timestamp' => post.ts
   )
   puts slack_res.body
