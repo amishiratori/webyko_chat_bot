37ab7b812b3308e8b36d2347fcb6ead601744ca8
amishiratori <shiratoriami@gmail.com>
2020-05-05 22:43:13 +0900

    fix auth

--- app.rb
+++ app.rb
@@ -6,47 +6,9 @@ require 'dotenv'
 require 'net/http'
 require 'slack-ruby-client'
 require 'google/apis/sheets_v4'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
-require 'fileutils'
 require './models'
 
 Dotenv.load
-
-OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
-APPLICATION_NAME = 'TEST'.freeze
-CREDENTIALS = {
-  web: {
-    client_id: ENV['CLIENT_ID'],
-    project_id: ENV['PROJECT_ID'],
-    auth_uri: ENV['AUTH_URI'],
-    token_uri: ENV['TOKEN_URI'],
-    auth_provider_x509_cert_url: ENV['CERT_URL'],
-    client_secret: ENV['CLIENT_SECRET'],
-    redirect_uris: [ENV['REDIRECT_URI']]
-  }
-}
-TOKEN_PATH = 'token.yaml'.freeze
-SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
-
-def authorize
-  client_id = Google::Auth::ClientId.from_hash CREDENTIALS
-  token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
-  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
-  user_id = "default"
-  credentials = authorizer.get_credentials user_id
-  if credentials.nil?
-    url = authorizer.get_authorization_url base_url: OOB_URI
-    puts "Open the following URL in the browser and enter the " \
-         "resulting code after authorization:\n" + url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: OOB_URI
-    )
-  end
-  credentials
-end
-
 
 post '/callback' do
   request_body = JSON.parse(request.body.read)
@@ -78,12 +40,13 @@ post '/callback' do
         trainee = Trainee.find_by(slack_name: name)
 
         service = Google::Apis::SheetsV4::SheetsService.new
-        service.client_options.application_name = APPLICATION_NAME
-        service.authorization = authorize
-        spreadsheet_id = ENV['SHEET_ID']
-        range = "Sheet1!J10"
-        response = service.get_spreadsheet_values(spreadsheet_id, range)
-        puts response.to_json
+        service.key = ENV['GOOGLE_API_KEY']
+        sheet_id = ENV['SHEET_ID']
+        range = 'J10'
+        response = service.get_spreadsheet_values(sheet_id, range)
+        response.values.each do |row|
+          puts row
+        end
       end
     elsif request_body['event']['channel'] == 'C012PCA7X1B' && request_body['event']['user'] != 'U012HRJKR6J'
       if request_body['event'].has_key?('text')
